2aa1f7610191911231fd12c8432d22ffe45cb3a8
unknown <caothangtrinh2002@gmail.com>
2023-11-30 16:28:42 +0700

    Add write output to python file

--- main.c
+++ main.c
@@ -30,9 +30,48 @@ void example_aes128_cbc(unsigned char* plaintext, int plaintext_len) {
     return;
 }
 
+void example_encrypt_to_python_file() {
+
+    rand_init();
+
+    unsigned char *key = random_block(16);
+    unsigned char *iv = random_block(16);
+
+    unsigned char pt[32] = {'A', 'E', 'S', '-', 'C', 'B', 'C', ' ', 'u', 's', 'e', 'd', ' ', 'f', 'o', 'r', ' ', 'i', 'm', 'a', 'g', 'e', ' ', 's', 'e', 'c', 'u', 'r', 'i', 't', 'y', '.'};
+
+    unsigned char *ct = aes_cbc_encrypt(pt, 32, key, iv);
+
+    int length_after_encrypt = length_after_pad(32);
+
+    FILE *fptr;
+
+    // Open a file in writing mode
+    fptr = fopen("output.py", "w");
+
+    // Write key
+    fprintf(fptr, "key = bytes.fromhex('");
+    for (int i = 0; i < 16; i++) 
+        fprintf(fptr, "%02x", key[i]);
+    fprintf(fptr, "')\n");
+
+    // Write iv
+    fprintf(fptr, "iv = bytes.fromhex('");
+    for (int i = 0; i < 16; i++) 
+        fprintf(fptr, "%02x", iv[i]);
+    fprintf(fptr, "')\n");
+
+    // Write ciphertext
+    fprintf(fptr, "ct = bytes.fromhex('");
+    for (int i = 0; i < length_after_encrypt; i++) 
+        fprintf(fptr, "%02x", ct[i]);
+    fprintf(fptr, "')\n");
+
+    // Close the file
+    fclose(fptr);
+}
+
 int main() {
-    test_aes(10000);
-    test_aes_cbc(100, 100);
-    test_pad_and_unpad(10000);
+
+    example_encrypt_to_python_file();
     return 0;
 }
